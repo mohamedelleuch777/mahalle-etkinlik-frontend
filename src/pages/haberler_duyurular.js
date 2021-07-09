f78d5eab6be92ee9f2f06e60dfa307adeae0bca5
walidzwari <w.zwari@gmail.com>
2021-07-09 23:08:54 +0100

    haberler page 40% done

--- src/pages/haberler_duyurular.js
+++ src/pages/haberler_duyurular.js
@@ -10,6 +10,7 @@ import { Container, Row, Col } from 'reactstrap';
 
 
 import img_clip from '../assets/img/image 12.png'
+import rectangle from '../assets/img/Rectangle 4@2x.png'
 
 const HaberlerDuyurular = (props) => {
     const [isMobile, setismobile] = useState(false);
@@ -26,11 +27,47 @@ const HaberlerDuyurular = (props) => {
             fontWeight: 500
         },
         paragraph: {
-            fontSize: 19,
-            color: "#7D8597",
+            color: "#666666",
+            fontFamily: 'Poppins',
+            textAlign: "regular",
+            // margin: "40px 0",
+            fontSize: 14,
+            marginTop:-15
+
+        },
+        img: {
+            width: "100%",
+            height: 'auto',
+            marginTop: 100,
+        },
+        path: {
+            listStyle: "none",
+            display: "flex",
+            padding: 0
+        },
+        item: {
+            // marginLeft: 15,
+            color: "#999999",
+            fontSize: 12,
             fontFamily: 'Poppins',
             textAlign: "center",
-            margin: "40px 0"
+            // margin: "60px 0",
+            fontWeight: 500
+        },
+        quote:{
+            color: "#0187CB",
+            fontSize:22,
+            fontFamily: 'Poppins',
+            marginTop:26.7
+            
+        },
+        bold:{
+            color: "#0187CB",
+            fontSize:60,
+            fontFamily: "'Roboto Condensed', sans-serif",
+            fontWeight:"bold",
+            lineHeight:"60px"
+
         }
     }
 
@@ -48,6 +85,24 @@ return(
         <TopBar />
         <Menu />
         {/* ---------------------------------------------------- */}
+        <Container style={{boxShadow:"#0000001A 0px 0px 15px 0px", padding:0}}>
+            <img src={rectangle} style={styles.img}/>
+            <Container style={{margin:"0 5%",width:"90%"}} >
+                <h3 style={{marginTop:"43.2px"}}>Zafer mahalle evi açıldı</h3>
+                <ul style={styles.path}>
+                    <li style={styles.item}>IBB</li>
+                    <li className="path-item" style={styles.item}>Haber ve Duyurular</li>
+                    <li className="path-item" style={styles.item}>14.06.2021</li>
+                </ul>
+                <p style={styles1.title}>Lorem Ipsum Nedir?</p>
+                <p style={styles.paragraph}>Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.</p>
+                <p style={styles1.title}>Neden Kullanırız?</p>
+                <p style={styles.paragraph}>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.</p>
+                <p style={styles1.title}>Nereden Gelir?</p>
+                <p style={styles.paragraph}>Yaygın inancın tersine, Lorem Ipsum rastgele sözcüklerden oluşmaz. Kökleri M.Ö. 45 tarihinden bu yana klasik Latin edebiyatına kadar uzanan 2000 yıllık bir geçmişi vardır. Virginia'daki Hampden-Sydney College'dan Latince profesörü Richard McClintock, bir Lorem Ipsum pasajında geçen ve anlaşılması en güç sözcüklerden biri olan 'consectetur' sözcüğünün klasik edebiyattaki örneklerini incelediğinde kesin bir kaynağa ulaşmıştır. Lorm Ipsum, Çiçero tarafından M.Ö. 45 tarihinde kaleme alınan "de Finibus Bonorum et Malorum" (İyi ve Kötünün Uç Sınırları) eserinin 1.10.32 ve 1.10.33 sayılı bölümlerinden gelmektedir. Bu kitap, ahlak kuramı üzerine bir tezdir ve Rönesans döneminde çok popüler olmuştur. Lorem Ipsum pasajının ilk satırı olan "Lorem ipsum dolor sit amet" 1.10.32 sayılı bölümdeki bir satırdan gelmektedir.</p>
+                <p className="paragraph-indented" style={styles.quote}>&#8222;1500'lerden beri kullanılmakta olan standard Lorem Ipsum metinleri ilgilenenler için yeniden üretilmiştir. Çiçero tarafından yazılan 1.10.32 ve 1.10.33 bölümleri de 1914 H. Rackham çevirisinden alınan İngilizce sürümleri eşliğinde özgün biçiminden yeniden üretilmiştir.</p>
+            </Container>
+        </Container>
         
             {/* ---------------------------------------------------- */}
         <Footer/>
@@ -98,7 +153,7 @@ const styles1 = {
         color: "#030D28",
         fontFamily: "Poppins",
         fontWeight: 600,
-        fontSize: 28,
+        fontSize: 14,
         textAlign: "left",
         // margin: 0,
         // minHeight: 45
